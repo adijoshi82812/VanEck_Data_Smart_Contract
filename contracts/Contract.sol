2d852d5a783f2d8bb3eaae407ded5ddc3049b9e5
adijoshi82812 <adijoshi82812@gmail.com>
2022-12-22 13:00:27 +0530

    Modified contract for storing multiple values

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -5,21 +5,39 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 contract Test is ChainlinkClient {
     using Chainlink for Chainlink.Request;
+    address private immutable owner;
 
-    uint256 private constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10;
-    string public Ticker;
-    string public ISIN;
-
-    constructor(address _linkAddress) {
-        setChainlinkToken(_linkAddress);
+    struct VanEck {
+        uint id;
+        string holdingName;
+        string ticker;
+        string ISIN;
     }
 
-    function requestTicker
-    (
-        address _oracle,
-        string memory _jobId,
-        string memory _id
-    ) public {
+    VanEck[] public data;
+
+    uint256 private constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10;
+    uint private counter = 0;
+
+    constructor() {
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        owner = msg.sender;
+    }
+
+    function requestData(address _oracle, string memory _jobId, string memory _id) external {
+        require(msg.sender == owner, "Only callable by owner");
+
+        requestTicker(_oracle, _jobId, _id);
+        requestISIN(_oracle, _jobId, _id);
+        requestHoldingName(_oracle, _jobId, _id);
+
+        counter += 1;
+        VanEck memory _data = VanEck(counter, "", "", "");
+
+        data.push(_data);
+    }
+
+    function requestTicker(address _oracle, string memory _jobId, string memory _id) internal {
         Chainlink.Request memory req = buildChainlinkRequest(
             stringToBytes32(_jobId),
             address(this),
@@ -31,12 +49,7 @@ contract Test is ChainlinkClient {
         sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
     }
 
-    function requestISIN
-    (
-        address _oracle,
-        string memory _jobId,
-        string memory _id
-    ) public {
+    function requestISIN(address _oracle, string memory _jobId, string memory _id) internal {
         Chainlink.Request memory req = buildChainlinkRequest(
             stringToBytes32(_jobId),
             address(this),
@@ -48,27 +61,32 @@ contract Test is ChainlinkClient {
         sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
     }
 
-    function fulfillTicker
-    (
-        bytes32 _requestID,
-        string memory _ticker
-    ) public recordChainlinkFulfillment(_requestID)
-    {
-        Ticker = _ticker;
+    function requestHoldingName(address _oracle, string memory _jobId, string memory _id) internal {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            stringToBytes32(_jobId),
+            address(this),
+            this.fulfillHoldingName.selector
+        );
+
+        req.add("id", _id);
+        req.add("path", "data,Holding Name");
+        sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
     }
 
-    function fulfillISIN
-    (
-        bytes32 _requestID,
-        string memory _ISIN
-    ) public recordChainlinkFulfillment(_requestID)
-    {
-        ISIN = _ISIN;
+    function fulfillTicker(bytes32 _requestID, string memory _ticker) external recordChainlinkFulfillment(_requestID) {
+        data[counter - 1].ticker = _ticker;
     }
 
-    function stringToBytes32(
-        string memory source
-    ) private pure returns (bytes32 result) {
+    function fulfillISIN(bytes32 _requestID, string memory _ISIN) external recordChainlinkFulfillment(_requestID) {
+        data[counter - 1].ISIN = _ISIN;
+    }
+
+    function fulfillHoldingName(bytes32 _requestID, string memory _holdingName) external
+        recordChainlinkFulfillment(_requestID) {
+            data[counter - 1].holdingName = _holdingName;
+    }
+
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
             return 0x0;
